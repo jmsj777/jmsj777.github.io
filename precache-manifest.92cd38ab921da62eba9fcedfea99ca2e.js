6c0af5d855fc0dfd054c8a2344d4ababdabe8add
José Mário da Silva Júnior <jmsj777@hotmail.com>
2019-06-11 23:11:01 -0300

    Updates

---
+++ precache-manifest.92cd38ab921da62eba9fcedfea99ca2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaacb1dadc1a80e795a072a743c849f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0210e2efc2688b9c583",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "4c71f575522ba3a068d1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b0210e2efc2688b9c583",
+    "url": "/static/js/2.b752aa5d.chunk.js"
+  },
+  {
+    "revision": "4c71f575522ba3a068d1",
+    "url": "/static/js/main.eaae4384.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
